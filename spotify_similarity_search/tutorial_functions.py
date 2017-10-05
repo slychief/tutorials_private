1ba618d1d141fcbd8571680c2df60d44e3565084
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2017-10-05 10:02:50 +0200

    Fix typos

--- spotify_similarity_search/tutorial_functions.py
+++ spotify_similarity_search/tutorial_functions.py
@@ -39,7 +39,7 @@ def get_playlist_metadata(spotify_client, playlists):
 
         # initialize fields for further processing
         playlist["track_ids"]   = []
-        
+
     return playlists
 
 
@@ -57,13 +57,13 @@ def get_track_ids(sp, playlists):
             limit = np.min([batch_size, playlist["num_tracks"] - offset])
 
             playlist_entries = sp.user_playlist_tracks(user        = playlist["user"],
-                                                       playlist_id = playlist["playlist_id"], 
-                                                       limit       = limit, 
+                                                       playlist_id = playlist["playlist_id"],
+                                                       limit       = limit,
                                                        offset      = offset,
                                                        fields      = ["items"])
 
             playlist["track_ids"].extend([entry["track"]["id"] for entry in playlist_entries["items"]])
-            
+
     return playlists
 
 
@@ -83,62 +83,62 @@ def aggregate_metadata(raw_track_data):
 
         # assamble metadata
         metadata.append([track_metadata["id"],
-                         artist_metadata["name"], 
-                         track_metadata["name"], 
+                         artist_metadata["name"],
+                         track_metadata["name"],
                          album_metadata["name"],
                          album_metadata["label"],
                          track_metadata["duration_ms"],
                          track_metadata["popularity"],
                          release_date,
-                         artist_metadata["genres"], 
+                         artist_metadata["genres"],
                          playlist_name])
 
-    metadata = pd.DataFrame(metadata, columns=["track_id", "artist_name", "title", "album_name", "label", 
+    metadata = pd.DataFrame(metadata, columns=["track_id", "artist_name", "title", "album_name", "label",
                                                "duration", "popularity",  "year",  "genres", "playlist"])
-    
+
     return metadata
 
 
 def aggregate_features(seq_data, track_data, metadata, with_year=False, with_popularity=False):
 
     calc_statistical_moments = lambda x: np.concatenate([x.mean(axis=0), x.std(axis=0)])
-    
+
     # sequential data
     segments = seq_data["segments"]
     sl       = len(segments)
-    
+
     # MFCCs - 24 dimensions
     mfcc              = np.array([s["timbre"] for s in segments])
     mfcc              = calc_statistical_moments(mfcc)
-    
+
     # Chroma / pitch classes - 24 dimensions
     chroma            = np.array([s["pitches"] for s in segments])
     chroma            = calc_statistical_moments(chroma)
-    
+
     # maximum loudness values per segment - 2 dimensions
     loudness_max      = np.array([s["loudness_max"] for s in segments]).reshape((sl,1))
     loudness_max      = calc_statistical_moments(loudness_max)
-    
+
     # offset of max loudness value within segment - 2 dimensions
     loudness_start    = np.array([s["loudness_start"] for s in segments]).reshape((sl,1))
     loudness_start    = calc_statistical_moments(loudness_start)
-    
+
     # length of max loudness values within segment - 2 dimensions
     loudness_max_time = np.array([s["loudness_max_time"] for s in segments]).reshape((sl,1))
     loudness_max_time = calc_statistical_moments(loudness_max_time)
-    
+
     # length of segment - 2 dimensions
     duration          = np.array([s["duration"] for s in segments]).reshape((sl,1))
     duration          = calc_statistical_moments(duration)
-    
+
     # confidence of segment boundary detection - 2 dimensions
     confidence        = np.array([s["confidence"] for s in segments]).reshape((sl,1))
     confidence        = calc_statistical_moments(confidence)
-    
+
     # concatenate sequential features
-    sequential_features = np.concatenate([mfcc, chroma, loudness_max, loudness_start, 
+    sequential_features = np.concatenate([mfcc, chroma, loudness_max, loudness_start,
                                           loudness_max_time, duration, confidence], axis=0)
-    
+
     # track-based data
     track_features = [track_data[0]["acousticness"],     # acoustic or not?
                       track_data[0]["danceability"],     # danceable?
@@ -149,14 +149,14 @@ def aggregate_features(seq_data, track_data, metadata, with_year=False, with_pop
                       track_data[0]["tempo"],            # slow or fast?
                       track_data[0]["time_signature"],   # 3/4, 4/4, 6/8, etc.
                       track_data[0]["valence"]]          # happy or sad?
-    
+
     if with_year:
         track_features.append(int(metadata["year"]))
-        
+
     if with_popularity:
         track_features.append(int(metadata["popularity"]))
-        
-    
+
+
     return np.concatenate([sequential_features, track_features], axis=0)
 
 
@@ -168,19 +168,19 @@ def aggregate_featuredata(raw_track_data, metadata):
 
         _, _, _, f_sequential, f_trackbased = spotify_data
 
-        feature_vec = aggregate_features(f_sequential, 
-                                         f_trackbased, 
-                                         metadata.iloc[i], 
-                                         with_year       = True, 
-                                         with_popularity = True)    
+        feature_vec = aggregate_features(f_sequential,
+                                         f_trackbased,
+                                         metadata.iloc[i],
+                                         with_year       = True,
+                                         with_popularity = True)
 
         feature_data.append(feature_vec)
 
     feature_data = np.asarray(feature_data)
-    
+
     return feature_data
-	
-	
+
+
 # updatable plot
 # a minimal example (sort of)
 
@@ -190,31 +190,31 @@ class PlotLosses(keras.callbacks.Callback):
         self.x = []
         self.losses = []
         self.val_losses = []
-        
+
         self.fig = plt.figure()
-        
+
         self.logs = []
 
     def on_epoch_end(self, epoch, logs={}):
-        
+
         self.logs.append(logs)
         self.x.append(self.i)
         self.losses.append(logs.get('loss'))
         self.val_losses.append(logs.get('val_loss'))
         self.i += 1
-        
+
         clear_output(wait=True)
         plt.plot(self.x, self.losses, label="loss")
         plt.plot(self.x, self.val_losses, label="val_loss")
         plt.legend()
         plt.show();
 
-		
+
 def contrastive_loss(y_true, y_pred):
     margin = 1
     return K.mean(y_true * K.square(y_pred) + (1 - y_true) * K.square(K.maximum(margin - y_pred, 0)))
-	
+
 def euclidean_distance(vects):
     x, y = vects
     return K.sqrt(K.maximum(K.sum(K.square(x - y), axis=1, keepdims=True), K.epsilon()))
-	
+
